dd3606a73f493530bc69902bee21dbec2f96edea
Tushar Upadhyay <tusharrockpg@gmail.com>
2021-05-22 21:25:40 +0530

    test

--- dev/email/urls.js
+++ dev/email/urls.js
@@ -1,4 +1,4 @@
-let baseURL = 'development'=='development'?'http://localhost:3001/':'https://ucointest.herokuapp.com/'
+let baseURL = process.env.NODE_ENV=='development'?'http://localhost:3001/':'https://ucointest.herokuapp.com/'
 let resetLink = baseURL + 'resetPassword'
 let verificationLink = baseURL + 'auth/verify'
 module.exports = {baseURL,resetLink,verificationLink};
